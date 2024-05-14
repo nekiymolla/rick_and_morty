44058a80eb5308553293dcd0e00879b5ceb8a9c9
Ivan Sobachkin <nekiymolla56@gmail.com>
2024-05-14 20:34:58 +0500

    core usecases

--- lib/feature/domain/usecases/get_persons_usecase.dart
+++ lib/feature/domain/usecases/get_persons_usecase.dart
@@ -1,12 +1,24 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_application_1/core/errors/error.dart';
+import 'package:flutter_application_1/core/errors/usecases.dart';
 import 'package:flutter_application_1/feature/domain/entities/person_entity.dart';
 import 'package:flutter_application_1/feature/domain/repository/person_repository.dart';
 
-class GetPersonsUseCase {
+class GetPersonsUseCase extends UseCases<List<PersonEntity>, PersonIdParams> {
   final PersonRepository personRepository;
   GetPersonsUseCase(this.personRepository);
-  Future<Either<Failure, List<PersonEntity>>> call(int page) async {
-    return await personRepository.getPersons(page);
+  @override
+  Future<Either<Failure, List<PersonEntity>>> call(
+      PersonIdParams params) async {
+    return await personRepository.getPersons(params.page);
   }
+}
+
+class PersonIdParams extends Equatable {
+  final int page;
+
+  const PersonIdParams({required this.page});
+  @override
+  List<Object?> get props => [page];
 }
